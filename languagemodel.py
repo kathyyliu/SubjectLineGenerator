ac56245db77a28fecd1af002580fb519e381b239
kathyyliu <kliu4@oxy.edu>
2021-12-04 16:25:18 -0800

    Bug in generation model

--- languagemodel.py
+++ languagemodel.py
@@ -1,23 +1,54 @@
-from keytotext import trainer, make_dataset
-import pandas
+import extractkeys
+from nltk.tokenize import word_tokenize
+from sklearn.model_selection import train_test_split
+from keytotext import trainer, pipeline
+import pandas as pd
 import json
 
 
-def train(subject_lines):
-    df = pandas.DataFrame(subject_lines)
+def label(subject_lines):
+    labeled = {
+        "subject line": [],
+        "keywords": []
+    }
+    # extract keys from subject lines as their labels
+    for line in subject_lines:
+        phrase = extractkeys.rake(line)[0]
+        tok_phrase = word_tokenize(phrase)
+        if len(tok_phrase) > 1:
+            x = extractkeys.head(tok_phrase, False)
+            if not x:
+                continue
+            keywords = [x]
+        else:
+            keywords = [tok_phrase[0]]
+        labeled["subject line"].append(line)
+        labeled["keywords"].append(keywords)
+    df = pd.DataFrame(labeled)
+    return df
+
+
+def train(labeled_data):
+    train, test = train_test_split(labeled_data, shuffle=True)
     model = trainer()
     model.from_pretrained()
-    model.train(train_df=df)
+    model.train(train_df=train, test_df=test, use_gpu=False)
+    return model
 
+
+def generate(model, keywords):
+    pass
 
 
 def main():
     f = open('data.json', )
     data = json.load(f)['emails']
-    subjectlines = []
+    subject_lines = []
     for email in data:
-        subjectlines.append(email[0][0])
-
+        subject_lines.append(email[0][0])
+    df = label(subject_lines)
+    model = train(df)
+    print(model.predict(["attend", "session"]))
 
 
 if __name__ == '__main__':
